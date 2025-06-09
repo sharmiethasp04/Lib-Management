ec20a22fbf4a02088fdf195e4cf0d8be48228802
sharmiethasp04 <u499128@wns.com>
2025-06-09 22:43:50 +0530

    Fourth commit

--- Program.cs
+++ Program.cs
@@ -4,23 +4,26 @@
 
 using LibraryManagement;
 // Book b1 = new Book { Title = "Pride and Prejudice", Author = "Jane Austin", ISBN="abc"};
+try
+{
+    Book b1 = new Book("HarryPotter", "J.K.Rowling", "567899", "hgf");
+    Magazine m1 = new Magazine("4567", "The Hindu", "45689");
+    Dvd d1 = new Dvd("6758", "ghjf", "fhjd");
+    b1.ShowInfo();
+    b1.DisplayDetails();
+    m1.ShowInfo();
+    m1.DisplayDetails();
+    d1.ShowInfo();
+    d1.DisplayDetails();
 
-Book b1 = new Book("HarryPotter", "J.K.Rowling", "567899", "hgf");
-Magazine m1 = new Magazine("4567", "The Hindu", "45689");
-Dvd d1 = new Dvd("6758", "ghjf", "fhjd");
 
-b1.ShowInfo();
-b1.DisplayDetails();
-m1.ShowInfo();
-m1.DisplayDetails();
-d1.ShowInfo();
-d1.DisplayDetails();
+}
 
 
-// catch(InvalidBookException ex)
-// {
-//     Console.WriteLine($"The error {ex.Message}");
-// }
+catch(InvalidBookException ex)
+{
+    Console.WriteLine($"The error {ex.Message}");
+}
 Console.WriteLine("Enter the overdue days");
 double overdue = Convert.ToInt32(Console.ReadLine());
 if (overdue > 15)
